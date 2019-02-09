f858db4cf77196c2de07d314b8128d749c58ac04
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-10 02:33:15 +0900

    update

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -82,9 +82,9 @@ def cnn_model_fn(features, labels, mode):
 
         optimizer = optimizers.SantaOptimizer(
             eta=1e-12,
-            gamma=0.8,
+            gamma=0.5,
             sigma=0.999,
-            const=1000,
+            const=1e+11,
             epsilon=1e-8,
             burnin=10000
         )
